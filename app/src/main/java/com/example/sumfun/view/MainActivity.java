1592f506f9949b1b077126a985a8123b9004fba1
Heather Stratton <hastratton@gmail.com>
2021-02-25 15:29:10 -0500

    Moved variables from MainActivity to PlayGameActivity

--- app/src/main/java/com/example/sumfun/view/MainActivity.java
+++ app/src/main/java/com/example/sumfun/view/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.sumfun.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.sumfun.R;
 import com.example.sumfun.presenter.Presenter;
@@ -13,15 +10,6 @@ import com.example.sumfun.presenter.Presenter;
 public class MainActivity extends AppCompatActivity {
     private Presenter presenter;
 
-    //for playGameActivity
-
-    private TextView first_num;
-    private TextView operator;
-    private TextView second_num;
-    private TextView equals;
-    private EditText response;
-    private int rand;
-    private int curLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +18,5 @@ public class MainActivity extends AppCompatActivity {
         presenter= new Presenter(this);
 
     }
-    public void displayToast(String message){
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
+
 }
--- app/src/main/java/com/example/sumfun/view/StatusActivity.java
+++ app/src/main/java/com/example/sumfun/view/StatusActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import com.example.sumfun.R;
+import com.example.sumfun.presenter.Presenter;
 
 public class StatusActivity extends AppCompatActivity {
+    private Presenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+
+
     }
 }
