c12a55f2b9422c1ac38b4cbeb3d75ab43edd1850
Heather Stratton <hastratton@gmail.com>
2021-02-25 15:50:23 -0500

    Added button that sends intent to game screen.  Removed rest of fragment files that were accidently added.

--- app/src/main/java/com/example/sumfun/view/MainActivity.java
+++ app/src/main/java/com/example/sumfun/view/MainActivity.java
@@ -2,21 +2,39 @@ package com.example.sumfun.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.sumfun.R;
 import com.example.sumfun.presenter.Presenter;
 
 public class MainActivity extends AppCompatActivity {
     private Presenter presenter;
+    private Button bPlay;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bPlay=findViewById(R.id.bPlay);
+
         presenter= new Presenter(this);
 
+        bPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handlePlayButton();
+
+            }
+        });
+
+    }
+    public void handlePlayButton(){
+        Intent intent = new Intent(this, PlayGameActivity.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/sumfun/view/PlayGameActivity.java
+++ app/src/main/java/com/example/sumfun/view/PlayGameActivity.java
@@ -13,6 +13,7 @@ import com.example.sumfun.presenter.Presenter;
 public class PlayGameActivity extends AppCompatActivity {
     private Presenter presenter;
     //for playGameActivity
+    private TextView gameScreen;
     private TextView first_num;
     private TextView operator;
     private TextView second_num;
@@ -25,6 +26,7 @@ public class PlayGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game);
+        gameScreen=findViewById(R.id.GameScreen);
     }
     public void displayToast(String message){
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
