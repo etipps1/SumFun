01d05f0dd735ce5bc7379c8395637709b741a89c
arojases <arielalejandrorojas@gmail.com>
2021-03-18 12:13:02 -0600

    Added anim folder, welcome screen with animation and congrats screen with animation

--- app/src/main/java/com/example/sumfun/view/LogoActivity.java
+++ app/src/main/java/com/example/sumfun/view/LogoActivity.java
@@ -3,6 +3,7 @@ package com.example.sumfun.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -12,32 +13,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sumfun.R;
 
-public class CongratsActivity extends AppCompatActivity {
+public class LogoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_congrats);
+
+        //Splash screen full screen without top bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_logo);
 
         //add animations
         Animation animation1 = AnimationUtils.loadAnimation(this, R.anim.up);
         Animation animation2 = AnimationUtils.loadAnimation(this, R.anim.down);
 
-        TextView commentTextView = findViewById(R.id.commentTextView);
-        TextView congratsTextView = findViewById(R.id.congratsTextView);
-        ImageView logoTextView = findViewById(R.id.logoTextView);
+        TextView byTextView = findViewById(R.id.byTextView);
+        TextView teamTextView = findViewById(R.id.teamTextView);
+        ImageView logoImageView = findViewById(R.id.logoImageView);
 
-        commentTextView.setAnimation(animation2);
-        congratsTextView.setAnimation(animation2);
-        logoTextView.setAnimation(animation1);
+        byTextView.setAnimation(animation2);
+        teamTextView.setAnimation(animation2);
+        logoImageView.setAnimation(animation1);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(CongratsActivity.this, MainActivity.class);
+                Intent intent = new Intent(LogoActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, 8000);
+        }, 4000);
     }
 }
