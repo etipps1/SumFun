fe30543b150e6aab64c5527945923269e2c3b918
Heather Stratton <hastratton@gmail.com>
2021-03-18 16:18:45 -0400

    fixed starCount after game completed

--- app/src/main/java/com/example/sumfun/presenter/PlayGamePresenter.java
+++ app/src/main/java/com/example/sumfun/presenter/PlayGamePresenter.java
@@ -344,6 +344,7 @@ public class PlayGamePresenter {
                 playGameActivity.displayCongrats();
                 currentLevel=1;
                 currentStage=0;
+                starCount=0;
                 op = "+";
             }
 
